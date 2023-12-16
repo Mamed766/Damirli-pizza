2345bfc4ff68bdb07a36efa473deb736dfc88c36
Mamed766 <mehemmeddemirli57@gmail.com>
2023-12-16 21:45:53 +0400

    Updated

---
+++ src/ui/Button.jsx
@@ -0,0 +1,36 @@
+import { Link } from 'react-router-dom';
+
+function Button({ children, disabled, to, type, onClick }) {
+  const base =
+    ' inline-block text-sm rounded-full bg-green-300  font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed   ';
+
+  const styles = {
+    primary: base + ' px-4 py-3 md:px-6 md:py-4',
+    small: base + ' px-4 py-2 md:px-6 md:py-4 text-xs',
+    secondary:
+      ' px-4 py-3 md:px-6 md:py-4 text-sm inline-block border-2 border-stone-300 rounded-full  font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed   ',
+    round: base + ' px-2.5 py-1 md:px-3.5 md:py-2 text-sm',
+  };
+
+  if (to)
+    return (
+      <Link to={to} className={styles[type]}>
+        {children}
+      </Link>
+    );
+
+  if (onClick)
+    return (
+      <button onClick={onClick} disabled={disabled} className={styles[type]}>
+        {children}
+      </button>
+    );
+
+  return (
+    <button disabled={disabled} className={styles[type]}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
